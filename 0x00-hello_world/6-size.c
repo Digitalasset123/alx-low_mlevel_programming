54f2193d468dfe5ca650a93c0b1dae272ee3cf49
Digitalasset123 <luckyjoshua321@gmail.com>
2023-02-16 15:44:22 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf(Size of a char : 1 byte(s), the sizeof(char))
+printf(Size of an int : 4 byte(s), sizeof(int))
+printf(Size of a long int : 8 byte(s), sizeof(long int))
+printf(Size of a long long int : 8 byte(s), sizeof(long long int))
+printf(Siize of a float : 4 byte(s), sizeof(float))
+return (0);
+}
