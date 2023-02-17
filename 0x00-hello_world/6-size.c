237b01d46fe8c8c8e628edb736062e0c77816c2c
Digitalasset123 <luckyjoshua321@gmail.com>
2023-02-16 19:13:35 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-printf(Size of a char: 1 byte(s)\n,the sizeof(char));
-printf(Size of an int: 4 byte(s)\n,sizeof(int));
-printf(Size of a long int: 8 byte(s)\n,sizeof(long int));
-printf(Size of a long long int: 8 byte(s)\n,sizeof(long long int));
-printf(Siize of a float: 4 byte(s)\n,sizeof(float));
+int main(void) 	
+{printf(Size of a char : 1 byte(s)sizeof(char));
+printf(Size of an int : 4 byte(s)sizeof(int));
+printf(Size of a long int : 8 byte(s)sizeof(long int));
+printf(Size of a long long int : 8 byte(s)sizeof(long long int));
+printf(Size of a float : 4 byte(s)sizeof(float));
 return (0);
 }
